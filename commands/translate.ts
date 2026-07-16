7699a837c31c0fbde8de3be62e9a4360097db23f
William Kang <william@wkang.ca>
2026-07-15 23:54:34 -0400

    update @william5553/translate-google-api

--- commands/translate.ts
+++ commands/translate.ts
@@ -2,6 +2,8 @@ import { ApplicationCommandOptionType, EmbedBuilder, InteractionContextType } fr
 import { translate, isSupported, langs, getCode } from '@william5553/translate-google-api';
 import type { CommandConfig, CommandModule } from '../types.ts';
 
+const getLanguageName = (code: string | undefined) => Object.entries(langs).find(([, languageCode]) => languageCode === code)?.[0] ?? code ?? 'Unknown';
+
 export const config = {
     name: 'translate',
     description: 'Translates text to another language using Google Translate.',
@@ -34,23 +36,31 @@ export const config = {
 } satisfies CommandConfig; // TODO: https://discordjs.guide/slash-commands/autocomplete.html#enabling-autocomplete
 
 export const execute: CommandModule['execute'] = async (_client, interaction) => {
-    if (interaction.options.getString('from') && !isSupported(interaction.options.getString('from')!)) return interaction.reply(`${interaction.options.getString('from')} isn't a supported language`);
-    if (interaction.options.getString('to') && !isSupported(interaction.options.getString('to')!)) return interaction.reply(`${interaction.options.getString('to')} isn't a supported language`);
+    const text = interaction.options.getString('text', true);
+    const from = interaction.options.getString('from');
+    const to = interaction.options.getString('to');
+
+    if (from && !isSupported(from)) return interaction.reply(`${from} isn't a supported language`);
+    if (to && !isSupported(to)) return interaction.reply(`${to} isn't a supported language`);
 
     await interaction.deferReply();
+    const targetLanguageName = getLanguageName(getCode(to ?? 'en'));
 
-    return translate(interaction.options.getString('text')!, { from: interaction.options.getString('from'), to: interaction.options.getString('to') })
+    return translate(text, {
+        ...from && { from },
+        ...to && { to }
+    })
         .then(result =>
             interaction.editReply({ embeds: [
                 new EmbedBuilder()
                     .setColor(0x53_90_F5)
                     .setTitle('Translation')
                     .addFields(
-                        { name: '**Input**', value: interaction.options.getString('text')!, inline: true },
+                        { name: '**Input**', value: text, inline: true },
                         { name: '**Text**', value: result.text, inline: true },
                         { name: '\n', value: '\n' },
-                        { name: '**From**', value: Object.keys(langs).find(key => langs[key] === result.from.language.iso)!, inline: true },
-                        { name: '**To**', value: Object.keys(langs).find(key => langs[key] === getCode(interaction.options.getString('to') ?? 'en'))!, inline: true }
+                        { name: '**From**', value: getLanguageName(result.from.language.iso), inline: true },
+                        { name: '**To**', value: targetLanguageName, inline: true }
                     )
                     .setTimestamp()
                     .setFooter({ text: 'Powered by Google Translate', iconURL: 'https://www.gstatic.com/images/branding/product/1x/translate_96dp.png' })
